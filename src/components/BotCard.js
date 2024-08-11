5e01a2e7d65f0711d3096137019e49e4e7a1b857
Megan <megan.nyakina@student.moringaschool.com>
2024-08-11 21:19:27 +0300

    adds file and default content

---
+++ src/components/BotCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BotCard() {
+  return (
+    <div>BotCard</div>
+  )
+}
+
+export default BotCard
---
+++ src/components/YourBotArmy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function YourBotArmy() {
+  return (
+    <div>YourBotArmy</div>
+  )
+}
+
+export default YourBotArmy
